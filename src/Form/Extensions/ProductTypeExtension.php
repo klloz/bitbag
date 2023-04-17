7b07a07e2c558c97a1981406030aaa8596c6defe
kloz <klaudia.lozowska@netinteractive.pl>
2023-04-17 16:10:20 +0200

    ProductType extended with color

---
+++ src/Form/Extensions/ProductTypeExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Extensions;
+
+use App\Entity\Product\Color;
+use App\Entity\Product\Product;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProductTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('color', ChoiceType::class, [
+                'choices' => Product::ALL_COLORS,
+                'required' => false,
+                'label' => 'Color', // todo use translations, for choices as well
+                'choice_label' => function ($key, $value) {
+                    return $key;
+                },
+            ]);
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ProductType::class];
+    }
+}
